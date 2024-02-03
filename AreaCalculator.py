be8139598a48e8aa22cfd8a450bf73555413130f
CodewitAyo <Ayo.odele01@gmail.com>
2024-02-03 09:55:15 -0600

    My calculator project

---
+++ AreaCalculator.py
@@ -0,0 +1,4 @@
+length1 = float(input("Enter your the length of your rectangle: "))
+width1 = float(input("Enter the width of your rectangle: "))
+Area = (length1 * width1)
+print("Your area = ", Area)
