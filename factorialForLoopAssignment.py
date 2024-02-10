946c1d5bf305f4ca4e9b19055a7eed8caaf7e329
CodewitAyo <Ayo.odele01@gmail.com>
2024-02-10 07:07:18 -0600

    A Python project that asks a user to enter a number, then finds the factorial of that number.

---
+++ factorialForLoopAssignment.py
@@ -0,0 +1,5 @@
+number = int(input("Enter in a number greater than or equal to 1: "))
+factorial = 1
+for i in range(1, number + 1):
+    factorial =
+print("The factorial of ", number, "is ", factorial, ".")
