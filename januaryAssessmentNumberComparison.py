da9bd374f0e2513adcaad44b7e8ec67a31379155
CodewitAyo <Ayo.odele01@gmail.com>
2024-02-10 07:15:05 -0600

    A Python project that asks a user to enter two numbers, then determines which of the two is larger.

---
+++ januaryAssessmentNumberComparison.py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter a second number: "))
+if num1 > num2:
+    print('The first number is larger.')
+elif num1 < num2:
+    print('The Second number is larger.')
+elif num1 == num2:
+    print('Both numbers are equal.')
+else:
+    print("Error.")
