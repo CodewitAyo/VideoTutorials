2019f4a4d4a63a301c34c4f525b1e30c4edaa0de
CodewitAyo <157227780+CodewitAyo@users.noreply.github.com>
2024-04-13 08:57:17 -0600

    Delete evenOdd.py

--- evenOdd.py
+++
@@ -1,9 +0,0 @@
-number = float(input("Enter in a number: "))
-if number % 2 == 0:
-    answer = "even"
-elif number % 2 == 1:
-    answer = "odd"
-else:
-    answer = "invalid"
-    print("incorrect syntax")
-print(number, "is an ", answer, " number")
