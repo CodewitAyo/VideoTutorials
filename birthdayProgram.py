fdec1b4a92a7b608c45214a3e76882a7d8fcb224
CodewitAyo <157227780+CodewitAyo@users.noreply.github.com>
2024-04-13 08:58:04 -0600

    Delete birthdayProgram.py

--- birthdayProgram.py
+++
@@ -1,4 +0,0 @@
-birth_year = int(input("Enter your birthyear:"))
-current_year = int(input("Enter in the current year: "))
-birthday = current_year - birth_year
-print("You are or will be ", birthday, " years old this year.")
