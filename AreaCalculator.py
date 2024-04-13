2647ca6266e5b9ede1472bfbd41ef14dbd0b50ec
CodewitAyo <157227780+CodewitAyo@users.noreply.github.com>
2024-04-13 08:58:38 -0600

    Delete AreaCalculator.py

--- AreaCalculator.py
+++
@@ -1,4 +0,0 @@
-length1 = float(input("Enter your the length of your rectangle: "))
-width1 = float(input("Enter the width of your rectangle: "))
-Area = (length1 * width1)
-print("Your area = ", Area)
